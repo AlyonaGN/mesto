3915c0636fa5d261b90890834f5c12e26821e16a
Alyona Gnevusheva <tchernovaalena@gmail.com>
2020-07-01 16:08:03 +0300

    Проектная работа 4 (добавлены: добавление, удаление фотографий, лайк, полноэкранный просмотр фотографий)

--- scripts/index.js
+++ scripts/index.js
@@ -1,48 +1,165 @@
-const popup = document.querySelector('.popup');
 const profile = document.querySelector('.profile');
-const popupForm = popup.querySelector('.popup__form');
-
-const popupOpenButton = profile.querySelector('.profile__edit-button');
-const popupCloseButton = popup.querySelector('.popup__close-button');
-const popupSaveButton =  popup.querySelector('.popup__submit-button');
-
-const nameInput = popup.querySelector('.popup__field_name');
-const descriptionInput = popup.querySelector('.popup__field_description');
-
+const popupEditProfile = document.querySelector('.popup_type_profile');
+const popupEditProfileForm = popupEditProfile.querySelector('.popup__form');
+const popupEditProfileOpenButton = profile.querySelector('.profile__edit-button');
+const popupEditProfileCloseButton = popupEditProfile.querySelector('.popup__close-button');
+const nameInput = popupEditProfile.querySelector('.popup__field_name');
+const profileDescriptionInput = popupEditProfile.querySelector('.popup__field_description');
 const profileUserName = profile.querySelector('.profile__name');
 const profileDescription = profile.querySelector('.profile__description');
 
+const popupAddPhoto = document.querySelector('.popup_type_add-photo');
+const popupAddPhotoForm = popupAddPhoto.querySelector('.popup__form');
+const popupAddPhotoOpenButton = profile.querySelector('.profile__add-button');
+const popupAddPhotoCloseButton = popupAddPhoto.querySelector('.popup__close-button');
+const newPhotoDescriptionInput = popupAddPhoto.querySelector('.popup__field_photo-description');
+const newPhotoLinkInput = popupAddPhoto.querySelector('.popup__field_photo-link');
+
+const popupPhotoView = document.querySelector('.popup_type_photo-view');
+const popupPhotoFullScreen = popupPhotoView.querySelector('.popup__photo-fullscreen');
+const popupPhotoViewCloseButton = popupPhotoView.querySelector('.popup__close-button');
+const popupPhotoFullScreenCaption = popupPhotoView.querySelector('.popup__photo-caption');
+const photoCardTemplate = document.querySelector('.photo-card-template').content;
+const photoCards = document.querySelector('.photo-cards__list');
+
+const initialCards = [
+    {
+        name: 'Архыз',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    },
+    {
+        name: 'Челябинская область',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    },
+    {
+        name: 'Иваново',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    },
+    {
+        name: 'Камчатка',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    },
+    {
+        name: 'Холмогорский район',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    },
+    {
+        name: 'Байкал',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    }
+];
+
+initialCards.reverse().forEach(function (initialCard) {
+    const photoDescription = initialCard.name;
+    const link = initialCard.link;
+    const initialPhotoCard = createPhotoCard(photoDescription, link);
+    addPhotoCard(initialPhotoCard);
+});
+
+function createPhotoCard (photoDescription, link) {
+    const photoCard = photoCardTemplate.cloneNode(true);
+    photoCard.querySelector('.photo-card__description').innerText = photoDescription;
+    photoCard.querySelector('.photo-card__photo').src = link;
+    
+    const deletePhotoCardButton = photoCard.querySelector('.photo-card__delete-button');
+    deletePhotoCardButton.addEventListener('click', deletePhotoCard);
+
+    const photoCardLikeButton = photoCard.querySelector('.photo-card__like');
+    photoCardLikeButton.addEventListener('click', toggleLikePhotoCard);
+
+    const photoCardPhoto = photoCard.querySelector('.photo-card__photo');
+    photoCardPhoto.addEventListener('click', openPhotoCardFullScreen);
+    
+    return photoCard;
+}
+
+function addPhotoCard (photoCard) {
+    photoCards.prepend(photoCard);
+}
+
+function addNewPhotoSubmitHandler(event) {
+    event.preventDefault();
+    event.stopPropagation();
+    
+    const newPhotoCard = createPhotoCard(newPhotoDescriptionInput.value, newPhotoLinkInput.value);
+    addPhotoCard(newPhotoCard);
+
+    popupClose(event);
+}
 
 function popupClose (event) { 
     event.preventDefault();
     event.stopPropagation();
-    popup.classList.remove('popup_opened');
+
+    if (event.target === popupEditProfileCloseButton || event.target === popupEditProfileForm) {
+        popupEditProfile.classList.remove('popup_opened');
+    }
+
+    else if (event.target === popupAddPhotoCloseButton || event.target === popupAddPhotoForm) {
+        popupAddPhoto.classList.remove('popup_opened');
+    }
+
+    else if (event.target === popupPhotoViewCloseButton) {
+        popupPhotoView.classList.remove('popup_opened');
+    }
 }
 
 function popupOpen (event) {
-    nameInput.value = profileUserName.textContent;
-    descriptionInput.value = profileDescription.textContent;
+    if (event.target === popupEditProfileOpenButton) {
+        
+        nameInput.value = profileUserName.textContent;
+        profileDescriptionInput.value = profileDescription.textContent;
 
-    popup.classList.add('popup_opened');
+        popupEditProfile.classList.add('popup_opened');
+    }
+
+    else if (event.target === popupAddPhotoOpenButton) {
+        popupAddPhoto.classList.add('popup_opened');
+    }
 }
 
-function formSubmitHandler (event) {
+function openPhotoCardFullScreen (event) {
+    event.preventDefault();
+    event.stopPropagation();
     
-    event.preventDefault(); 
+    const photoCardFullScreen = event.target.closest('.photo-card');
+    popupPhotoFullScreen.src = photoCardFullScreen.querySelector('.photo-card__photo').src;
+    popupPhotoFullScreenCaption.innerText = photoCardFullScreen.querySelector('.photo-card__description').innerText;
+
+    popupPhotoView.classList.add('popup_opened');
+}
+
+function editProfileFormSubmitHandler (event) {
+    event.preventDefault();
+    event.stopPropagation(); 
   
     profileUserName.textContent = nameInput.value;
-    profileDescription.textContent = descriptionInput.value;
+    profileDescription.textContent = profileDescriptionInput.value;
         
     popupClose(event);
 }
 
-function stopPropagation (event) {
-    event.stopPropagation();
+function toggleLikePhotoCard (event) {
+    event.target.classList.toggle('photo-card__like_active');
 }
 
-popupOpenButton.addEventListener('click', popupOpen);
-popupCloseButton.addEventListener('click', popupClose);
-popupForm.addEventListener('click', stopPropagation);
-popup.addEventListener('click', popupClose);
-popupForm.addEventListener('submit', formSubmitHandler);
+function deletePhotoCard (event) {
+    const photoCardToBeDeleted = event.target.closest('li');
+    photoCardToBeDeleted.remove();
+}
+
+
+popupEditProfileOpenButton.addEventListener('click', popupOpen);
+popupEditProfileCloseButton.addEventListener('click', popupClose);
+
+popupAddPhotoOpenButton.addEventListener('click', popupOpen);
+popupAddPhotoCloseButton.addEventListener('click', popupClose);
+
+popupEditProfileForm.addEventListener('submit', editProfileFormSubmitHandler);
+popupAddPhotoForm.addEventListener('submit', addNewPhotoSubmitHandler);
+
+popupPhotoViewCloseButton.addEventListener('click', popupClose);
+
+
+
 
