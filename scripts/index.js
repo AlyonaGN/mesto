50b9481675fa7ca5996da8c156cea5c6a01dfa2f
Alyona Gnevusheva <tchernovaalena@gmail.com>
2020-07-31 09:35:01 +0300

    refactor: улучшена внутренняя логика импортов

--- scripts/index.js
+++ scripts/index.js
@@ -1,25 +1,31 @@
 import { Card } from './Card.js';
-import { preparePopupProfileForOpening, preparePopupAddPhotoForOpening, addPhotoCard, closePopup, editProfileFormSubmitHandler, 
-    profile, popupEditProfileForm, popupAddPhotoForm, addNewPhotoSubmitHandler, createCard } from './utils.js';
 import { FormValidator } from './FormValidator.js';
+import { openPopup, closePopup } from './utils.js';
+import { validationConfig } from './config.js';
+
+const popupEditProfile = document.querySelector('.popup_type_profile');
+const popupEditProfileForm = popupEditProfile.querySelector('.popup__form');
+const profile = document.querySelector('.profile');
+const nameInput = popupEditProfile.querySelector('.popup__field_name');
+const profileDescriptionInput = popupEditProfile.querySelector('.popup__field_description');
+const profileUserName = profile.querySelector('.profile__name');
+const profileDescription = profile.querySelector('.profile__description');
+const popupEditProfileOpenButton = profile.querySelector('.profile__edit-button');
+
+const popupAddPhoto = document.querySelector('.popup_type_add-photo');
+const popupAddPhotoForm = popupAddPhoto.querySelector('.popup__form');
+const popupAddPhotoSubmitButton = popupAddPhoto.querySelector('.popup__submit-button');
+const newPhotoDescriptionInput = popupAddPhoto.querySelector('.popup__field_photo-description');
+const newPhotoLinkInput = popupAddPhoto.querySelector('.popup__field_photo-link');
+const popupAddPhotoOpenButton = profile.querySelector('.profile__add-button');
+const photoCardTemplateClass = '.photo-card-template';
+
+const inactiveSumbitButtonSelector = 'popup__submit-button_inactive';
 
 const popupList = document.querySelectorAll('.popup');
-const popupEditProfileOpenButton = profile.querySelector('.profile__edit-button');
-const popupAddPhotoOpenButton = profile.querySelector('.profile__add-button');
-
-const validationConfig = {
-    formSelector: '.popup__form',
-    inputSelector: '.popup__input',
-    fieldSelector: '.popup__field',
-    submitButtonSelector: '.popup__submit-button',
-    inactiveButtonClass: 'popup__submit-button_inactive',
-    errorClass: '.popup__field-error',
-    fieldErrorSelector: 'popup__field_type_error',
-    inputErrorClass: 'popup__input_type_error',
-  };
 
 initialCards.reverse().forEach((item) => {
-    const cardElement = createCard(Card, item.name, item.link);
+    const cardElement = createCard(item.name, item.link);
     addPhotoCard(cardElement);
   });
 
@@ -31,13 +37,67 @@ popupList.forEach(function (popup) {
     });
 });
 
+function openProfilePopup(profileFormValidator) {
+    nameInput.value = profileUserName.textContent;
+    profileDescriptionInput.value = profileDescription.textContent;
+    profileFormValidator.hidePopupErrors(popupEditProfile);
+    openPopup(popupEditProfile);
+}
+
+function openAddPhotoPopup(addPhotoFormValidator) {
+    newPhotoDescriptionInput.value = '';
+    newPhotoLinkInput.value = '';
+    makeButtonInactive(popupAddPhotoSubmitButton, inactiveSumbitButtonSelector);
+    addPhotoFormValidator.hidePopupErrors(popupAddPhoto);
+    openPopup(popupAddPhoto);
+}
+
+function createCard(cardName, cardLink) {
+    const newPhotoCard = new Card(photoCardTemplateClass, cardName, cardLink);
+    return newPhotoCard.generateCard();
+}
+
+function addNewPhotoSubmitHandler(event) {
+    event.preventDefault();
+    event.stopPropagation();
+
+    const newCardElement = createCard(newPhotoDescriptionInput.value, newPhotoLinkInput.value);
+    addPhotoCard(newCardElement);
+
+    const popup = event.target.closest('.popup');
+    closePopup(popup);
+
+    newPhotoDescriptionInput.value = '';
+    newPhotoLinkInput.value = '';
+}
+
+function makeButtonInactive(buttonElement, inactiveButtonSelector) {
+    buttonElement.classList.add(inactiveButtonSelector);
+    buttonElement.setAttribute('disabled', true);
+  }
+
+function addPhotoCard(photoCard) {
+    const photoCards = document.querySelector('.photo-cards__list');
+    photoCards.prepend(photoCard);
+}
+
+function editProfileFormSubmitHandler(event) {
+    event.preventDefault();
+    event.stopPropagation();
+
+    profileUserName.textContent = nameInput.value;
+    profileDescription.textContent = profileDescriptionInput.value;
+    const popup = event.target.closest('.popup');
+    closePopup(popup);
+}
+
 const profileFormValidator = new FormValidator(validationConfig, popupEditProfileForm);
 const addPhotoFormValidator = new FormValidator(validationConfig, popupAddPhotoForm);
 profileFormValidator.enableValidation();
 addPhotoFormValidator.enableValidation();
 
 popupEditProfileForm.addEventListener('submit', editProfileFormSubmitHandler);
-popupAddPhotoForm.addEventListener('submit', () => { addNewPhotoSubmitHandler(Card, event) });
-popupEditProfileOpenButton.addEventListener('click', () => { preparePopupProfileForOpening(profileFormValidator) });
-popupAddPhotoOpenButton.addEventListener('click', () => { preparePopupAddPhotoForOpening(addPhotoFormValidator) });
+popupAddPhotoForm.addEventListener('submit', () => { addNewPhotoSubmitHandler(event) });
+popupEditProfileOpenButton.addEventListener('click', () => { openProfilePopup(profileFormValidator) });
+popupAddPhotoOpenButton.addEventListener('click', () => { openAddPhotoPopup(addPhotoFormValidator) });
 
